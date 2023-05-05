63553562817ae7613a9662e0212d384446ea3c75
PRAUDThomas <thomaspraud44@gmail.com>
2023-05-05 15:43:58 +0200

    create folder header + footer + components header done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,10 @@
-import reactLogo from '../../assets/react.svg';
 import './styles.scss';
+import Header from '../Header';
 
 function App() {
   return (
     <div className="app">
-      <h1 className="app__title">Vite + React</h1>
-      <img src={reactLogo} alt="react logo" />
+      <Header />
     </div>
   );
 }
