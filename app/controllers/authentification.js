54eef85e51c583af868d489c43d609e5eedcf246
inTheMembrane <naughtnaught@outlook.com>
2023-05-05 17:06:43 +0200

    CHORE wip authentification controller

--- app/controllers/authentification.js
+++ app/controllers/authentification.js
@@ -28,65 +28,33 @@ module.exports = {
     /**
      * User controller to get a record.
      * ExpressMiddleware signature
-     * @param {object} req Express req.object (not used)
+     * @param {object} req Express req.object
      * @param {object} res Express response object
      * @returns Route API JSON response
      */
-    async getOne(req, res) {
-        const user = await returnRecordOrThrowError(req.params.id);
+    async login(req, res) {
+        const user = await userDataMapper.findByPk(req.params.id);
         return res.json(user);
     },
     /**
-     * User controller to update a record.
+     * User controller to create a record.
      * ExpressMiddleware signature
-     * @param {object} req Express req.object (not used)
+     * @param {object} req Express req.object
      * @param {object} res Express response object
      * @returns Route API JSON response
      */
-    async update(req, res) {
-        const user = await returnRecordOrThrowError(req.params.id);
+    async signup(req, res) {
+        if (req.body.password !== req.body.passwordCheck) {
 
-        // Si l'email fait parti des éléments mis à jour, on check si cet email n'est pas utilisé par un autre user
-        if (req.body.email) {
-            const userWithSameCredentials = await userDataMapper.isUnique(
-                req.body,
-                req.params.id
-            );
-            if (userWithSameCredentials) {
-                throw new Error(`Other user already exists with this email`, {
-                    statusCode: 400,
-                });
-            }
         }
-        const modifiedUser = {
-            email: req.body.email ?? user.email,
-            password: req.body.password ?? user.password,
-            firstName: req.body.firstName ?? user.firstName,
-            lastName: req.body.lastName ?? user.lastName,
-        };
-        const savedUser = await userDataMapper.update(
-            req.params.id,
-            modifiedUser
-        );
+        const user = await userDataMapper.isUnique(req.body);
+        if (user) {
+            throw new Error(`User already exists with this email`, { statusCode: 400 });
+        }
+        
+
+        const savedUser = await userDataMapper.insert(req.body);
         return res.json(savedUser);
     },
 
-    /**
-     * User controller to delete a record.
-     * ExpressMiddleware signature
-     * @param {object} req Express req.object (not used)
-     * @param {object} res Express response object
-     * @returns Route API JSON response
-     */
-    async delete(req, res) {
-        // deleted sera un booléen TRUE si deletion success
-        const deleted = await userDataMapper.delete(req.params.id);
-        if (!deleted) {
-            throw new ApiError('This user does not exists', {
-                statusCode: 404,
-            });
-        }
-
-        return res.status(204).json();
-    },
 };
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,4 @@
 const userController = require('./user');
 const authentificationController = require('./authentification');
 
-
 module.exports = { userController, authentificationController };
