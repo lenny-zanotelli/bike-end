8ece6c090f72b54c19a26f27f0a7deb92b0a250a
PRAUDThomas <thomaspraud44@gmail.com>
2023-05-09 15:47:51 +0200

    feat_intergation_TODO_mui

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,15 @@
 import './styles.scss';
 import Header from '../Header';
 import Footer from '../Footer';
+import LoginPage from '../loginPage';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <LoginPage />
       <Footer />
+
     </div>
   );
 }
