2373684f54bbaf16c63542a5ed31cd4e97d8ce51
PRAUDThomas <thomaspraud44@gmail.com>
2023-05-10 09:19:27 +0200

    feat_signup-page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,13 @@
 import './styles.scss';
 import Header from '../Header';
 import Footer from '../Footer';
+import SignupPage from '../SignupPage';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <SignupPage />
       <Footer />
     </div>
   );
