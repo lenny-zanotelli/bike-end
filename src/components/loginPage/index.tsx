fbad362848bb859e2d91c989496f0a39adbce339
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-05-11 10:24:04 +0200

    feat: add style in mui and delete soon the scss file + loginPage is reponsive

--- src/components/loginPage/index.tsx
+++ src/components/loginPage/index.tsx
@@ -4,6 +4,41 @@ import {
 } from '@mui/material';
 import { ChangeEvent, FormEvent, useState } from 'react';
 
+const styles = {
+  containerConnect: {
+    width: '70vw',
+    mt: '3rem',
+    borderRadius: '5px',
+    p: '1rem',
+    textAlign: 'center',
+    backgroundColor: 'rgb(154, 183, 192, 0.3)',
+    '@media only screen and (min-device-width : 768px)': {
+      maxWidth: '35%',
+    },
+  },
+  containerConnectTitle: {
+    fontSize: '2rem',
+    py: '1rem',
+    px: '2rem',
+  },
+  input: {
+    width: '80%',
+    my: '0.5rem',
+    backgroundColor: 'white',
+  },
+  containerConnectFormBtn: {
+    my: '1rem',
+    backgroundColor: '#207868',
+    ':hover': {
+      backgroundColor: '#0b5447',
+    },
+  },
+  containerConnectForgotPassword: {
+    my: '1rem',
+    fontSize: '0.7rem',
+  },
+};
+
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -27,12 +62,23 @@ function LoginPage() {
 
     <Container
       className="container"
+      component="main"
+      maxWidth="lg"
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
     >
       <Box
         className="container__connect"
+        component="section"
+        sx={styles.containerConnect}
       >
         <Typography
           className="container__connect__title"
+          component="h2"
+          sx={styles.containerConnectTitle}
         >
           Connexion
         </Typography>
@@ -42,7 +88,8 @@ function LoginPage() {
           onSubmit={handleSubmit}
         >
           <TextField
-            sx={{ width: '80%' }}
+            sx={styles.input}
+            color="success"
             variant="outlined"
             margin="normal"
             required
@@ -53,7 +100,8 @@ function LoginPage() {
             onChange={handleChangeEmail}
           />
           <TextField
-            sx={{ width: '80%' }}
+            sx={styles.input}
+            color="success"
             variant="outlined"
             margin="normal"
             required
@@ -65,18 +113,19 @@ function LoginPage() {
             onChange={handleChangePassword}
           />
           <Button
-            sx={{ width: '30%' }}
+            className="container__connect__form-btn"
+            sx={styles.containerConnectFormBtn}
             type="submit"
-            fullWidth
+            size="large"
             variant="contained"
-            color="primary"
           >
             Valider
           </Button>
         </form>
         <Button
           className="container__connect__forgotPassword"
-          href="/"
+          size="small"
+          sx={styles.containerConnectForgotPassword}
         >
           Mot de passe oublié ?
         </Button>
