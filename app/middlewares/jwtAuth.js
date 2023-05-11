0392db11a9757696072e08fe3c22fe18f69faabf
inTheMembrane <naughtnaught@outlook.com>
2023-05-11 17:05:40 +0200

    REFACTOR homogenise module exports

--- app/middlewares/jwtAuth.js
+++ app/middlewares/jwtAuth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const { jwtAuth } = require('.');
 
 const jwtAuth = (req, res, next) => {
     const token = req.headers.authorization?.replace('Bearer ', '');
