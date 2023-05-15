221878c72e621af8ac0293b4592e2a3774319b5b
lenny-zanotelli <96521135+lenny-zanotelli@users.noreply.github.com>
2023-05-15 14:50:15 +0200

    Dev to Main Branch - squash and Merge integration Pages
    * chore: integration folder add
    
    * style: accueil page html structure  mobile + css done
    
    * chore: delete accueil.html, not use.
    
    * style: connection page html structure mobile + desktop + css done
    
    * style: accueil desktop css done
    
    * style: createAccount page html structure mobile + desktop + css done
    
    * style: aboutUs page html structure mobile + desktop + css done
    
    * style: resolve issue with resolution on 'accueil.html'
    
    * style: modal create in html and js
    
    * style: integration myAccout mobile done
    
    * style: integration of myAccount page + css responsive done
    
    * chore: modify svg bike logo
    
    * build_MUI_dependency
    
    * FEAT_header-footer-component
    
    * feat_signup-page
    
    * feat_signup-page
    
    * refactor: harmonize footer height and font size on connexion page
    
    * refactor: harmonize footer for all html page
    
    * style: hover animation on button Rejoindre l'Aventure accueil
    
    * style: add style to modal in myAccount page and harmonizing of all css/html integration
    
    * style: searchPage header done and delete of css in html on other html file
    
    * chore: rename file for harmonization
    
    * style: add icon inputs for search city
    
    * feat: add mobile first style on searchPage integration
    
    * feat: add desktop style on searh page integration
    
    * refactor: delete blank line
    
    * refactor: change variable name, typing on handleEvent
    
    * feat: add material ui style in signupPage, responsive ok
    
    * style: add a modification for global styles, no scroll with overflow hidden
    
    * feat: homePage wip - HomeHeader from Home Component is done and responsive
    
    * feat: mobile view for HomePage in React+mui done
    
    * feat: desktop view for HomePage in React+mui done
    
    * refactor: harmonize footer height and font size on connexion page
    
    * refactor: harmonize footer for all html page
    
    * style: hover animation on button Rejoindre l'Aventure accueil
    
    * style: add style to modal in myAccount page and harmonizing of all css/html integration
    
    * style: searchPage header done and delete of css in html on other html file
    
    * chore: rename file for harmonization
    
    * style: add icon inputs for search city
    
    * feat: add mobile first style on searchPage integration
    
    * feat: add desktop style on searh page integration
    
    * refactor: delete blank line
    
    * feat_loginPage_commponent-integration
    
    * feat_intergation_TODO_mui
    
    * feat_intergation_TODO_mui
    
    * refacto: add mui component no style at the moment
    
    * feat: add style in mui and delete soon the scss file + loginPage is reponsive
    
    ---------
    
    Co-authored-by: PRAUDThomas <thomaspraud44@gmail.com>
    Co-authored-by: Thomas PRAUD <118735605+PRAUDThomas@users.noreply.github.com>

---
+++ src/components/Home/HomeBody/index.tsx
@@ -0,0 +1,83 @@
+import { Box, Button, Typography } from '@mui/material';
+
+const styles = {
+  containerBody: {
+    m: '2rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '@media screen and (min-width: 768px)': {
+      marginTop: '4rem',
+    },
+  },
+  bodyContent: {
+    textAlign: 'center',
+    fontSize: '1rem',
+    py: '0.5rem',
+    '@media screen and (min-width: 768px)': {
+      fontSize: '1.15rem',
+      fontWeight: 'bold',
+    },
+  },
+};
+
+function HomeBody() {
+  return (
+
+    <Box
+      className="container__body"
+      component="main"
+      sx={styles.containerBody}
+    >
+      <Typography
+        className="body__content"
+        paragraph
+        sx={styles.bodyContent}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aperiam odit,
+        praesentium saepe qui eveniet
+        amet facere ratione vel distinctio.
+      </Typography>
+      {/* router pas encore fait, je laisse un anchor pour l'instant */}
+      <a href="#">
+        <Button
+          className="body__create-account-btn"
+          variant="contained"
+          size="large"
+          sx={{
+            backgroundColor: '#207868',
+            ':hover': {
+              backgroundColor: '#0b5447',
+					  },
+          }}
+        >
+          Rejoindre l&apos;Aventure
+        </Button>
+      </a>
+      {/* router pas encore fait, je laisse un anchor pour l'instant */}
+      <a href="#">
+        <Button
+          className="body__connect-btn"
+          variant="text"
+          size="small"
+          sx={{
+					  fontSize: '0.7rem',
+            color: '#788896',
+            textDecoration: 'underline',
+            textDecorationColor: '#CED4D9',
+            textUnderlineOffset: '3px',
+            my: '0.5rem',
+          }}
+        >
+          Se connecter
+        </Button>
+      </a>
+
+    </Box>
+
+  );
+}
+
+export default HomeBody;
---
+++ src/components/Home/HomeHeader/index.tsx
@@ -0,0 +1,47 @@
+import { Box, Typography } from '@mui/material';
+import banner from '../../../assets/images/hero-banner.jpg';
+
+const styles = {
+  headerContainer: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    height: '50vh',
+  },
+  headerTitle: {
+    position: 'absolute',
+    fontSize: '3.5rem',
+    fontWeight: 'bolder',
+    fontStyle: 'italic',
+    '@media screen and (min-width: 768px)': {
+      fontSize: '8rem',
+    },
+  },
+};
+
+function HomeHeader() {
+  return (
+    <Box
+      className="header__container"
+      component="header"
+      sx={styles.headerContainer}
+    >
+      <img
+        className="header__banner"
+        src={banner}
+        alt="hero banner"
+      />
+      <Typography
+        className="header__title"
+        variant="h1"
+        sx={styles.headerTitle}
+      >
+        BIKE END
+      </Typography>
+    </Box>
+
+  );
+}
+
+export default HomeHeader;
