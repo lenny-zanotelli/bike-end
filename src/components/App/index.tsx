260100ff121c35c4d02446fcea373bd8f19ccc13
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-05-16 16:10:48 +0200

    feat: add searchPage when user is connected

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,16 +1,28 @@
 import './styles.scss';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 import Home from '../Home';
 import SignupPage from '../SignupPage';
 import LoginPage from '../loginPage';
 import ErrorPage from '../Error';
+import SearchPage from '../searchPage';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { updateLoginStatus } from '../../store/reducers/login';
 
 function App() {
   const location = useLocation();
+  const dispatch = useAppDispatch();
 
   const isHomePage = location.pathname === '/';
+  const isLogged = useAppSelector((state) => state.login.logged);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const isUserLogged = Boolean(token);
+    dispatch(updateLoginStatus(isUserLogged));
+  });
 
   return (
     <div className="app">
@@ -18,7 +30,7 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<Home />}
+          element={isLogged ? <SearchPage /> : <Home />}
         />
         <Route
           path="/signup"
---
+++ src/components/searchPage/index.tsx
@@ -0,0 +1,7 @@
+function searchPage() {
+  return (
+    <h2> C&apos;est la page de recherche</h2>
+  );
+}
+
+export default searchPage;
