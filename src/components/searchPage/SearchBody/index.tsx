3c1e7edfb63e4aabd3ebe3fccd541869fd6a8bff
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-05-17 15:48:04 +0200

    feat: fix desktop and mobile view + add mui-image dependencies

--- src/components/searchPage/SearchBody/index.tsx
+++ src/components/searchPage/SearchBody/index.tsx
@@ -1,17 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import {
   Button,
-  Container, InputAdornment, TextField, Typography,
+  Container, Divider, InputAdornment, TextField, Typography,
 } from '@mui/material';
+import { Image } from 'mui-image';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import EuroRoundedIcon from '@mui/icons-material/EuroRounded';
 import UpdateRoundedIcon from '@mui/icons-material/UpdateRounded';
+import testImg from '../../../assets/images/test-searchPage-desktop.jpg';
 
 const styles = {
+  container: {
+    '@media only screen and (min-device-width : 768px)': {
+      maxWidth: '50%',
+      // marginLeft: '25%',
+    },
+  },
   containerTitle: {
-    fontSize: '1.2rem',
+    fontSize: '1.5rem',
     textAlign: 'center',
     fontWeight: 'bold',
     mt: '2rem',
+    '@media only screen and (min-device-width : 768px)': {
+      fontWeight: 'bolder',
+      fontSize: '1.8rem',
+      paddingBottom: '0.5rem',
+    },
   },
   containerSearchForm: {
     display: 'flex',
@@ -53,143 +67,214 @@ const styles = {
     mt: '1rem',
     gap: '0.8rem',
   },
+  desktopContainer: {
+    '@media only screen and (min-device-width : 768px)': {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  },
+  desktopContainerImg: {
+    '@media only screen and (min-device-width : 768px)': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  },
+  containerSubtitle: {
+    '@media only screen and (min-device-width : 768px)': {
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      fontSize: '2rem',
+    },
+  },
+  underline: {
+    display: 'none',
+    '@media only screen and (min-device-width : 768px)': {
+      color: '#9EADBA',
+      display: 'block',
+    },
+  },
+
 } as const;
 
 function SearchBody() {
   return (
     <Container
-      component="main"
-      className="container"
+      className="desktop-container"
+      sx={styles.desktopContainer}
     >
-      <Typography
-        component="h1"
-        className="container__title"
-        sx={styles.containerTitle}
-      >
-        De nouveaux horizons à découvrir. En selle !
-      </Typography>
       <Container
-        component="section"
-        className="container__search-form"
-        sx={styles.containerSearchForm}
+        component="main"
+        className="container"
+        sx={styles.container}
       >
-        <TextField
-          className="search-form__input-city"
-          fullWidth
-          name="city-start"
-          placeholder="Lieu de départ"
-          aria-placeholder="Lieu de départ"
-          color="success"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <LocationOnIcon />
-              </InputAdornment>
-            ),
-          }}
+        <Typography
+          component="h1"
+          className="container__title"
+          sx={styles.containerTitle}
+        >
+          De nouveaux horizons à découvrir. En selle !
+        </Typography>
+        <Typography
+          component="h2"
+          className="container__subtitle hidden"
+          sx={styles.containerSubtitle}
+        >
+          Saisissez votre voyage
+        </Typography>
+        <Divider
+          className="underline"
+          sx={styles.underline}
         />
-        <TextField
-          className="search-form__input-city"
-          fullWidth
-          name="city-end"
-          placeholder="Lieu d'arrivé"
-          aria-placeholder="Lieu d'arrivé"
-          color="success"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <LocationOnIcon />
-              </InputAdornment>
-            ),
-          }}
+        <Container
+          component="section"
+          className="container__search-form"
+          sx={styles.containerSearchForm}
+        >
+          <TextField
+            className="search-form__input-city"
+            fullWidth
+            name="city-start"
+            placeholder="Lieu de départ"
+            aria-placeholder="Lieu de départ"
+            color="success"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <LocationOnIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            className="search-form__input-city"
+            fullWidth
+            name="city-end"
+            placeholder="Lieu d'arrivé"
+            aria-placeholder="Lieu d'arrivé"
+            color="success"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <LocationOnIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Container
+            component="article"
+            className="search-form__filter-section"
+            disableGutters
+            sx={styles.searchFormFilterSection}
+          >
+            <TextField
+              fullWidth
+              className="search-form__input-filter"
+              sx={{ marginRight: '0.6rem' }}
+              type="date"
+              name="date"
+              color="success"
+            />
+            <TextField
+              fullWidth
+              className="search-form__input-filter"
+              type="time"
+              name="time"
+              color="success"
+            />
+          </Container>
+        </Container>
+        <Container
+          component="section"
+          className="container__filter-form"
         />
         <Container
           component="article"
-          className="search-form__filter-section"
-          sx={styles.searchFormFilterSection}
+          className="filter-form__criteria"
+          sx={styles.filterFormCriteria}
         >
+          <Typography
+            component="h2"
+            className="filter-form__title"
+            sx={styles.filterFormTitle}
+          >
+            Filtres :
+          </Typography>
           <TextField
-            className="search-form__input-filter"
-            type="date"
-            name="date"
-            color="success"
+            size="small"
+            type="number"
+            placeholder="Budget"
+            className="filter-form__input_euro"
+            sx={styles.filterFormInput}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <EuroRoundedIcon />
+                </InputAdornment>
+              ),
+            }}
           />
           <TextField
-            className="search-form__input-filter"
-            type="time"
-            name="time"
-            color="success"
+            size="small"
+            className="filter-form__input_journey-travel"
+            type="text"
+            placeholder="Temps de trajet"
+            sx={styles.filterFormInput}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <UpdateRoundedIcon />
+                </InputAdornment>
+              ),
+            }}
           />
+        </Container>
+        <Container
+          component="article"
+          className="filter-form__btn"
+          sx={styles.filterFormBtn}
+        >
+          <Button
+            className="filter-form__btn-search"
+            type="button"
+            size="large"
+            variant="contained"
+            color="success"
+          >
+            Recherche
+          </Button>
+          <Button
+            className="filter-form__btn-reset"
+            type="button"
+            size="large"
+            variant="contained"
+            color="error"
+          >
+            Réinitialiser
+          </Button>
         </Container>
       </Container>
       <Container
-        component="section"
-        className="container__filter-form"
-      />
-      <Container
-        component="article"
-        className="filter-form__criteria"
-        sx={styles.filterFormCriteria}
+        className="desktop-container__img"
+        sx={styles.desktopContainerImg}
       >
-        <Typography
-          component="h2"
-          className="filter-form__title"
-          sx={styles.filterFormTitle}
-        >
-          Filtres :
-        </Typography>
-        <TextField
-          size="small"
-          type="number"
-          placeholder="Budget"
-          className="filter-form__input_euro"
-          sx={styles.filterFormInput}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <EuroRoundedIcon />
-              </InputAdornment>
-            ),
-          }}
+        <Image
+          className="desktop-img hidden"
+          src={testImg}
+          alt="town"
+          width={550}
+          height={500}
+          fit="cover"
+          duration={1500}
+          easing="cubic-bezier(0.7, 0, 0.6, 1)"
+          shift="left"
+          distance="500px"
+          shiftDuration={900}
+          style={{ paddingLeft: '6rem', marginTop: '9rem' }}
         />
-        <TextField
-          size="small"
-          className="filter-form__input_journey-travel"
-          type="text"
-          placeholder="Temps de trajet"
-          sx={styles.filterFormInput}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <UpdateRoundedIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Container>
-      <Container
-        component="article"
-        className="filter-form__btn"
-        sx={styles.filterFormBtn}
-      >
-        <Button
-          className="filter-form__btn-search"
-          type="button"
-          size="large"
-          variant="contained"
-          color="success"
-        >
-          Recherche
-        </Button>
-        <Button
-          className="filter-form__btn-reset"
-          type="button"
-          size="large"
-          variant="contained"
-          color="error"
-        >
-          Réinitialiser
-        </Button>
+
       </Container>
     </Container>
 
