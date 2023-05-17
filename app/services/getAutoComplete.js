388d27636b70cde9f1a05b2eb970724d30d9f53c
franckvarnava <franckvarnava@gmail.com>
2023-05-17 16:15:47 +0200

    REFACTO auto-complete search places

--- app/services/getAutoComplete.js
+++ app/services/getAutoComplete.js
@@ -3,10 +3,12 @@ const axios = require('axios');
 module.exports = {
     async findAll(queryParam) {
         try {
+            // On reconstruit l'url nécessaire à notre requête
             const URLtoFetch = process.env.SNCF_API_URL_NAVITIA +
                 '/places?q=' +
                 queryParam +
                 '&type[]=address&type[]=stop_area&depth=1&count=100';
+            // On lance la requête get via axios avec le token valide
             const data = await axios.get(URLtoFetch,
                 {
                     headers: {
@@ -14,6 +16,7 @@ module.exports = {
                     }
                 }
             );
+            // On renvoie uniquement un tableau de "places"
             return data.data.places;
         } catch (error) {
             console.log(error);
