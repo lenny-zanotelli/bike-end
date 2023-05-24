b938b7d47c5a81cd0fd923cb803d3cebf0255a7b
inTheMembrane <naughtnaught@outlook.com>
2023-05-24 18:43:10 +0200

    FIX error handling

--- app/middlewares/jwtAuth.js
+++ app/middlewares/jwtAuth.js
@@ -3,35 +3,23 @@ const jwt = require('jsonwebtoken');
 const jwtAuth = (req, res, next) => {
     try {
         const token = req.headers.authorization?.replace('Bearer ', '');
-        const data = jwt.verify(token, process.env.AUTH_API_SECRET);
-        console.log('data : ',data)
+        if (!token) {
+                    return res.status(401).json('Invalid token');
+                }
+        const userData = jwt.verify(token, process.env.AUTH_API_SECRET);
+        if (!userData) {
+            return res.status(401).json('Invalid token');
+        }
+        console.log('Verified user : ',userData)
         // on renseigne dans notre requete l'id du user afin de parametrer les requetes
-        req.userId = data.userId;
+        req.userId = userData.userId;
         next();
     } catch (error) {
-        error.status=401
+        error.status=500
         error.type = 'validating token'
         next(error)
     }
 };
 
-// const jwtAuth = (req, res, next) => {
-//     const token = req.headers.authorization?.replace('Bearer ', '');
-//     if (!token) {
-//         return res.status(401).json('Invalid token');
-//     }
-//     try {
-//         const user = jwt.verify(token, process.env.AUTH_API_SECRET);
-//         if (!user) {
-//             return res.status(401).json('Invalid token');
-//         }
-//         // on renseigne dans notre requete l'id du user afin de parametrer les requetes
-//         req.userId = user.id;
-//         next();
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json('Internal error');
-//     }
-// };
 
 module.exports = { jwtAuth };
