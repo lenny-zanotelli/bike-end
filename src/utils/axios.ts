6017d0aeccd75a6c1cc1808ae70c64467108208d
inTheMembrane <naughtnaught@outlook.com>
2023-06-06 11:55:23 +0200

    CHORE try token async

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+const getToken = async () => {
+  const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
+  return tokenWithoutQuotes;
+};
 // eslint-disable-next-line import/prefer-default-export
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_PRIVATE_API_URL,
   headers: {
-    Authorization: `Bearer ${await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1')}`,
+    Authorization: `Bearer ${getToken()}`,
   },
 });
