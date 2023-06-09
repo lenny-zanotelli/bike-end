42361e4008bd6c67dd6575ad439eb85087cce5c5
inTheMembrane <naughtnaught@outlook.com>
2023-06-09 18:42:36 +0200

    FIX minimum 2 caracters for name and surname

--- app/validation/schemas/userCreateSchema.js
+++ app/validation/schemas/userCreateSchema.js
@@ -5,7 +5,7 @@ const regex = require('../../config/regex');
 module.exports = Joi.object({
     email: Joi.string().regex(RegExp(regex.email)).required(),
     password: Joi.string().regex(RegExp(regex.password)).required(),
-    firstname: Joi.string().min(2).required(),
-    lastname: Joi.string().min(2).required(),
+    firstname: Joi.string().min(1).required(),
+    lastname: Joi.string().min(1).required(),
     acceptedConditions: Joi.boolean().strict().valid(true).required(),
 }).required();
--- app/validation/schemas/userUpdateSchema.js
+++ app/validation/schemas/userUpdateSchema.js
@@ -5,6 +5,6 @@ const regex = require('../../config/regex');
 module.exports = Joi.object({
     email: Joi.string().regex(RegExp(regex.email)),
     password: Joi.string().regex(RegExp(regex.password)),
-    firstname: Joi.string().min(2),
-    lastname: Joi.string().min(2),
+    firstname: Joi.string().min(1),
+    lastname: Joi.string().min(1),
 }).required();
