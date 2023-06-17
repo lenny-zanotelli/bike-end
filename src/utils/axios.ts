2442360bdcb4bde324c21a2631969bb3e16e0252
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-06-17 15:08:21 +0200

    fix: fix axios for jwt verify in backend

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,32 @@
+// import axios from 'axios';
+
+// const getToken = async () => {
+//   const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
+//   return tokenWithoutQuotes;
+// };
+// // eslint-disable-next-line import/prefer-default-export
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_PRIVATE_API_URL,
+//   headers: {
+//     Authorization: `Bearer ${getToken()}`,
+//   },
+// });
+
 import axios from 'axios';
 
-const getToken = async () => {
-  const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
-  return tokenWithoutQuotes;
-};
 // eslint-disable-next-line import/prefer-default-export
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_PRIVATE_API_URL,
-  headers: {
-    Authorization: `Bearer ${getToken()}`,
-  },
+});
+
+async function getToken() {
+  const tokenWithoutQuotes = await localStorage.getItem('token')?.replace(/^"(.*)"$/, '$1');
+  return tokenWithoutQuotes;
+}
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const tokenWithoutQuotes = await getToken();
+  const updatedConfig = { ...config }; // Créer une copie de l'objet config
+  updatedConfig.headers.Authorization = `Bearer ${tokenWithoutQuotes}`;
+  return updatedConfig;
 });
